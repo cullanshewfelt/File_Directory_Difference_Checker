1f64c127ee57724b75bff0cc07e8719ffbe92fdf
Cullan Shewfelt <cullanrocks@gmail.com>
2019-07-11 10:34:20 -0700

    The file directory difference checker will parse data to check for descrepencies in master backups of files with the same name, but different extensions. A very esoteric tool that was designed to help my workflow at work.

---
+++ diff_checker.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+// This script is to be used if there are descrpencies in any of the master catalog backups,
+// ,or instance, if number of files don't match up.
+
+// use "ls -R" in bi_music/wav to generate list of files. Copy output to wav_list.txt
+// then run this script. copy the output to wav_no_ext.txt
+// then run "diff wav_no_ext.txt mp3_no_ext.txt" and it will spit out the differences.
+
+const filePath = path.join(__dirname, 'source/wav_list.txt');
+
+let songNames = [];
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(filePath)
+});
+
+lineReader.on('line', function (line) {
+  if(line.includes('DLM - ')){
+    // console.log(line.split('.')[0]);
+    songNames.push(line.split('.')[0])
+  }
+});
+
+lineReader.on('close', () => {
+  songNames.sort();
+  songNames.forEach(song => {
+    console.log(song)
+  })
+})
